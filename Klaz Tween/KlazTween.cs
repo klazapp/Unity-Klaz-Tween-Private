b70d9e06b68d0404cb1582bfef546e817528b88d
klazapp <klazapp@klazapp.com>
2024-01-22 16:02:25 +0800

    Update KlazTween.cs

--- Klaz Tween/KlazTween.cs
+++ Klaz Tween/KlazTween.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public delegate void TweenCallback();
 
+//TODO : UseJobSystem should be a private variable
 //TODO : Manager should be a singleton
 //TODO : All current values should be clamped from start to end, particularly job system values
 //TODO : Try to find a way to create a generic job system
